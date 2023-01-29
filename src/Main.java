0c630a1385fdc35704359fc24721aec39c5aeb0f
Zaalyona <zaalyona@yandex.ru>
2023-01-29 23:13:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //основные задачи
+        //первая
+        byte firstVariable = 5;
+        short secondVariable = 256;
+        int thirdVariable = 1000000;
+        long fourthVariable = 20000000000L;
+
+        float fifthVariable = 1.5684F;
+        double sixthVariable = 5.87965354D;
+
+        char seventhVariable = 'a';
+
+        boolean eighthVariable = secondVariable < 78;
+
+        System.out.println("Первая переменная = " + firstVariable);
+        System.out.println("Вторая переменная = " + secondVariable);
+        System.out.println("Третья переменная = " + thirdVariable);
+        System.out.println("Четвертая переменная = " + fourthVariable);
+        System.out.println("Пятая переменная = " + fifthVariable);
+        System.out.println("Шестая переменная = " + sixthVariable);
+        System.out.println("Седьмая переменная = " + seventhVariable);
+        System.out.println("Восьмая переменная = " + eighthVariable);
+
+        //вторая
+    }
+}
